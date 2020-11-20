22352c5418d472c6e83a25803e81d4e8679dc7cb
Lmcabu <74607371+Lmcabu@users.noreply.github.com>
2020-11-20 22:53:59 +0800

    Update Op.java

--- Comp2021Project/src/hk/edu/polyu/comp/comp2021/g17/cvfs/model/criterion/Op.java
+++ Comp2021Project/src/hk/edu/polyu/comp/comp2021/g17/cvfs/model/criterion/Op.java
@@ -5,13 +5,43 @@ public enum Op{
 	equals,
 	G,//greater
 	GE,//greater or equal
-	L,LE,E,NE,
+	
+	//丰蕴霖同学   这L,LE我改成S，SE
+	S,SE,E,NE,
 	not_contains,
 	AND, OR,
 	not_equals;
 	public Op negate() {
 		//TODO
 		//return the negate version of op
+		switch (this){
+			case contains:
+				return not_contains;
+			case equals:
+				return not_equals;
+			case G:
+				return SE;
+			case GE:
+				return S;
+			case S:
+				return GE;
+			case SE:
+				return G;
+			case E:
+				return NE;
+			case NE:
+				return E;
+			case not_contains:
+				return contains;
+			case AND:
+				return OR;
+			case OR:
+				return AND;
+			case not_equals:
+				return equals;
+		}
 		return null;
 	}
+		
+
 }
